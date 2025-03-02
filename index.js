4a152a82bf282c154b1a655c0ffaeacd4b17c404
QAISER ALI <qaisers.official@gmail.com>
2025-03-02 23:00:55 +0500

    Add post editing functionality with corresponding views and routes

--- index.js
+++ index.js
@@ -50,6 +50,11 @@ app.get('/like/:id', isLoggedIn, async (req, res) => {
   res.redirect("/profile");
 });
 
+app.get('/edit/:id', isLoggedIn, async (req, res) => {
+  let post = await postModel.findOne({_id: req.params.id}).populate("user");
+  res.render("edit", {post});
+});
+
 app.post('/post', isLoggedIn, async (req, res) => {
   let user = await userModel.findOne({email: req.user.email});
   let {content} = req.body;
@@ -103,6 +108,11 @@ app.post('/login', async(req, res) => {
       res.redirect("profile")
     }
   });
+});
+
+app.post('/update/:id', isLoggedIn, async (req, res) => {
+  let post = await postModel.findOneAndUpdate({_id: req.params.id}, {content: req.body.content});
+  res.redirect("/profile");
 });
 
 // middleware to check if user is logged in
